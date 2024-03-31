af38f3caa5c05a581a435520f22b49d745cdec97
shanetaylor <taylor2230@gmail.com>
2024-03-30 21:04:11 -0500

    Exploded column and JSON schema fix with dynamic schemas

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,16 @@ import data.pipeline.Pipeline
 object Main {
   def main(args: Array[String]): Unit = {
     val simpleJsonString: String = {
-      """{"game":"bloons", "age": 2, "players":[1,2,3]}"""
+      """{"game":"bloons", "age": 2, "players":[1,2,3], "col_2": 2}"""
     }
 
     val jsonString: String = {
-      """{"col_1":"Loki", "col_2":2, "col_3":[1,2,3], "col_4":{"subcol_4A": "A", "subcol_4B": "B"}}"""
+      """{"game":"doge", "col_1":"Loki", "col_2":2, "col_3":[1,2,3], "col_4":{"subcol_4A": "A", "subcol_4B": "B"}}"""
     }
 
-    val f = Pipeline.Source.fromJson(Seq(jsonString))
+    val f = Pipeline.Source.fromJson(Seq(simpleJsonString, jsonString))
+
+    f.display()
 
     println(Pipeline.Sink.toJson(f))
   }
